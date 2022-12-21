23240287ce0d7075ab1cb1618b423a620cb34331
ruslan imshenetskiy <pilgrim2590@gmail.com>
2022-12-21 22:57:00 +0200

    rob11

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=32079865-e2e12c38828ecac1e7059915f&q=${Value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    `https://pixabay.com/api/?key=32079865-e2e12c38828ecac1e7059915f&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
